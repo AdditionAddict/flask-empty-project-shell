518c6eddb5913c370eb994f3570c48ab4fe85828
unknown <andrewallen1.6180@yahoo.co.uk>
2019-04-01 14:21:53 +0100

    used Flask-CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from config import config
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+
 
 db = SQLAlchemy()
 
@@ -11,6 +14,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     db.init_app(app)
+    CORS(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
