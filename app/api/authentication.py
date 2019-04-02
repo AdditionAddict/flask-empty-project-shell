27ec09c35bc190389e2a5424ad2685e53626f037
unknown <andrewallen1.6180@yahoo.co.uk>
2019-04-02 15:33:20 +0100

    login back to post

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -29,7 +29,7 @@ def verify_password(name_or_token, password):
 def auth_error():
     return jsonify({'success': False})
 
-@api.route('/login', methods=['GET'])
+@api.route('/login', methods=['POST'])
 @auth.login_required
 def get_token():
     print('login route')
